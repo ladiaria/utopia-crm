bc75d3d82a5d211056310d09226d23753a24ad49
Andrés Trífero <atrifero@ladiaria.com.uy>
2021-02-03 19:46:06 -0300

    Added new command to be able to bill subscriptions daily

--- local_settings_sample.py
+++ local_settings_sample.py
@@ -124,7 +124,8 @@ INVOICES_PATH = "path/to/folder/in/media/"
 # Set the payment types that you want to track on labels here. They must be set by type on a string.
 LABEL_INVOICE_PAYMENT_TYPES = "RI"
 
-# Set this to true to show only one item per subscription in subscription invoices.
+# Set this to true to show only one item per subscription in subscription invoices, instead of one item per
+# invoiceitem.
 USE_SQUASHED_SUBSCRIPTION_INVOICEITEMS = True
 
 # Use your own logo for the admin and dashboard
