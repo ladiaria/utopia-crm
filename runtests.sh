808b98fe610489d100138dda2cd28bd7d0edba26
Anibal <apacheco.uy@gmail.com>
2021-07-04 19:37:59 -0300

    runtests.sh upd

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,7 @@
-#!/bin/zsh
+#!/bin/sh
 export DJANGO_SETTINGS_MODULE="test_settings"
-python manage.py test tests.test_contact tests.test_dynamiccontactfilter tests.test_invoicing tests.test_subscriptor
+TEST_MODULES="tests.test_contact tests.test_dynamiccontactfilter tests.test_invoicing tests.test_subscriptor"
+# exit now if we are being sourced by another script or shell
+[[ "${#BASH_SOURCE[@]}" -gt "1" ]] && { return 0; }
+python -W ignore manage.py test ${TEST_MODULES}
 
