d50e226c120fa6bb4465107360555ff0df8f7b39
Andrés Trífero <atrifero@ladiaria.com.uy>
2021-07-07 13:36:02 -0300

    new template tag to add extra items on menus as needed

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from core.models import SubscriptionProduct
+from django.template.loader_tags import do_include
+from django.template.defaulttags import CommentNode
 
 register = template.Library()
 
@@ -47,3 +49,29 @@ def get_address_id_from_subscription(subscription_id, product_slug):
         return sp.address.id
     except Exception:
         return ''
+
+
+@register.tag('include_if_exists')
+def do_include_maybe(parser, token):
+    "Source: http://stackoverflow.com/a/18951166/15690"
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise template.TemplateSyntaxError(
+            "%r tag takes at least one argument: "
+            "the name of the template to be included." % bits[0])
+
+    try:
+        silent_node = do_include(parser, token)
+    except template.TemplateDoesNotExist:
+        # Django < 1.7
+        return CommentNode()
+
+    _orig_render = silent_node.render
+
+    def wrapped_render(*args, **kwargs):
+        try:
+            return _orig_render(*args, **kwargs)
+        except template.TemplateDoesNotExist:
+            return CommentNode()
+    silent_node.render = wrapped_render
+    return silent_node
