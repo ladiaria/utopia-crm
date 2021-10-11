7fb147742a00e1289225eb2d10fbacfb2690a87e
Andrés Trífero <atrifero@ladiaria.com.uy>
2021-10-11 19:16:36 -0300

    added a seller filter

--- support/views.py
+++ support/views.py
@@ -39,7 +39,7 @@ from core.models import (
 )
 from core.choices import CAMPAIGN_RESOLUTION_REASONS_CHOICES
 
-from .filters import IssueFilter, InvoicingIssueFilter, ScheduledActivityFilter
+from .filters import IssueFilter, InvoicingIssueFilter, ScheduledActivityFilter, ContactCampaignStatusFilter
 from .forms import *
 from .models import Seller, ScheduledTask, IssueStatus
 from core.utils import calc_price_from_products, process_products
@@ -2179,13 +2179,17 @@ def campaign_statistics_list(request):
 @staff_member_required
 def campaign_statistics_detail(request, campaign_id):
     campaign = get_object_or_404(Campaign, pk=campaign_id)
+    ccs_queryset = campaign.contactcampaignstatus_set.all()
+    ccs_filter = ContactCampaignStatusFilter(request.GET, queryset=ccs_queryset)
+    assigned_count = campaign.contactcampaignstatus_set.filter(seller__isnull=False).count()
+    not_assigned_count = campaign.contactcampaignstatus_set.filter(seller__isnull=True).count()
     total_count = campaign.contactcampaignstatus_set.count()
-    not_contacted_yet_count = campaign.contactcampaignstatus_set.filter(status=1).count()
-    tried_to_contact_count = campaign.contactcampaignstatus_set.filter(status=3).count()
-    contacted_count = campaign.contactcampaignstatus_set.filter(status__in=[4, 2]).count()
-    could_not_contact_count = campaign.contactcampaignstatus_set.filter(status__in=[5]).count()
+    not_contacted_yet_count = ccs_filter.qs.filter(status=1).count()
+    tried_to_contact_count = ccs_filter.qs.filter(status=3).count()
+    contacted_count = ccs_filter.qs.filter(status__in=[4, 2]).count()
+    could_not_contact_count = ccs_filter.qs.filter(status__in=[5]).count()
 
-    ccs_with_resolution = campaign.contactcampaignstatus_set.filter(campaign_resolution__isnull=False)
+    ccs_with_resolution = ccs_filter.qs.filter(campaign_resolution__isnull=False)
     ccs_with_resolution_count = ccs_with_resolution.count()
     success_with_direct_sale_count = ccs_with_resolution.filter(campaign_resolution="S2").count()
     success_with_promotion_count = ccs_with_resolution.filter(campaign_resolution="S1").count()
@@ -2213,31 +2217,35 @@ def campaign_statistics_detail(request, campaign_id):
 
     return render(request, "campaign_statistics_detail.html", {
         "campaign": campaign,
+        "filter": ccs_filter,
+        "filtered_count": ccs_filter.qs.count(),
         "total_count": total_count,
+        "assigned_count": assigned_count,
+        "not_assigned_count": not_assigned_count,
         "not_contacted_yet_count": not_contacted_yet_count,
-        "not_contacted_yet_pct": (not_contacted_yet_count * 100) / total_count,
+        "not_contacted_yet_pct": (not_contacted_yet_count * 100) / (total_count or 1),
         "tried_to_contact_count": tried_to_contact_count,
-        "tried_to_contact_pct": (tried_to_contact_count * 100) / total_count,
+        "tried_to_contact_pct": (tried_to_contact_count * 100) / (total_count or 1),
         "contacted_count": contacted_count,
-        "contacted_pct": (contacted_count * 100) / total_count,
+        "contacted_pct": (contacted_count * 100) / (total_count or 1),
         "could_not_contact_count": could_not_contact_count,
-        "could_not_contact_pct": (could_not_contact_count * 100) / total_count,
+        "could_not_contact_pct": (could_not_contact_count * 100) / (total_count or 1),
         "total_rejects_count": total_rejects_count,
-        "total_rejects_pct": (total_rejects_count * 100) / ccs_with_resolution_count,
+        "total_rejects_pct": (total_rejects_count * 100) / (ccs_with_resolution_count or 1),
         "rejects_by_reason": rejects_by_reason,
         "rejects_without_reason_count": rejects_without_reason_count,
         "success_with_promotion_count": success_with_promotion_count,
-        "success_with_promotion_pct": (success_with_promotion_count * 100) / ccs_with_resolution_count,
+        "success_with_promotion_pct": (success_with_promotion_count * 100) / (ccs_with_resolution_count or 1),
         "success_with_direct_sale_count": success_with_direct_sale_count,
-        "success_with_direct_sale_pct": (success_with_direct_sale_count * 100) / ccs_with_resolution_count,
+        "success_with_direct_sale_pct": (success_with_direct_sale_count * 100) / (ccs_with_resolution_count or 1),
         "scheduled_count": scheduled_count,
-        "scheduled_pct": (scheduled_count * 100) / ccs_with_resolution_count,
+        "scheduled_pct": (scheduled_count * 100) / (ccs_with_resolution_count or 1),
         "call_later_count": call_later_count,
-        "call_later_pct": (call_later_count * 100) / ccs_with_resolution_count,
+        "call_later_pct": (call_later_count * 100) / (ccs_with_resolution_count or 1),
         "unreachable_count": unreachable_count,
-        "unreachable_pct": (unreachable_count * 100) / ccs_with_resolution_count,
+        "unreachable_pct": (unreachable_count * 100) / (ccs_with_resolution_count or 1),
         "started_promotion_count": started_promotion_count,
-        "started_promotion_pct": (started_promotion_count * 100) / ccs_with_resolution_count,
+        "started_promotion_pct": (started_promotion_count * 100) / (ccs_with_resolution_count or 1),
         "error_in_promotion_count": error_in_promotion_count,
-        "error_in_promotion_pct": (error_in_promotion_count * 100) / ccs_with_resolution_count,
+        "error_in_promotion_pct": (error_in_promotion_count * 100) / (ccs_with_resolution_count or 1),
     })
