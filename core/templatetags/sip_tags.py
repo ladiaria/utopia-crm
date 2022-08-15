19cdda34bf3beca74105e8b4a16f36ef8298244f
Andrés Trífero <atrifero@ladiaria.com.uy>
2022-08-15 18:05:06 -0300

    rework of sip_tags

--- core/templatetags/sip_tags.py
+++ core/templatetags/sip_tags.py
@@ -1,26 +1,12 @@
 # coding=utf-8
 from django import template
 from django.conf import settings
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
-class Sip(template.Node):
-    """
-    Funciona de la siguiente forma: {% sip objeto.atributo %}
-    Donde objeto es una variable del contexto y atributo es un atributo de
-    dicha variable conteniendo un numero telefonico.
-    Seria facil modificar luego para aceptar tambien numeros como texto, etc
-    """
-    def __init__(self, token):
-        self.obj, self.attr = token.split('.')
-
-    def render(self, context):
-        number = getattr(context[self.obj], self.attr)
-        return '%s <a class="button btn-sm btn-primary" href="sip://%s%s"><i class="fas fa-phone"></i> Llamar</a>' % (
-            number, getattr(settings, 'SIP_DIALOUT', ''), number.split('/')[0]) if number else ''
-
-
-@register.tag(name='sip')
-def do_sip_tag(parser, token):
-    return Sip(token.contents.split()[1])
+@register.filter(name='sip')
+def sip_filter(value):
+    return mark_safe('%s <a class="button btn-sm btn-primary" href="sip://%s%s"><i class="fas fa-phone"></i> Llamar</a>' % (
+        value, getattr(settings, 'SIP_DIALOUT', ''), value.split('/')[0]) if value else '')
