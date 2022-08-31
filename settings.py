c5bee51ff8abed32812b772bf2a66ff314db76b2
Trífero <andres.trifero@gmail.com>
2022-08-31 17:07:16 -0300

    middleware for simple history

--- settings.py
+++ settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "simple_history.middleware.HistoryRequestMiddleware",  disabled until population
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 ROOT_URLCONF = "urls"
