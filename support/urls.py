dc66953acf309cd25bb4a5813f204f8bba8518a8
Tanya <andres.trifero@gmail.com>
2023-09-18 11:30:28 -0300

    inubicables

--- support/urls.py
+++ support/urls.py
@@ -51,6 +51,7 @@ from support.views import (
     edit_address_complementary_information,
     upload_do_not_call_numbers,
     tag_contacts,
+    not_contacted_campaign,
 )
 
 from django.urls import path, re_path
@@ -160,4 +161,5 @@ urlpatterns = [
     path("scheduled_task_filter/", scheduled_task_filter, name="scheduled_task_filter"),
     path("upload_do_not_call_numbers/", upload_do_not_call_numbers, name="upload_do_not_call_numbers"),
     path("tag_contacts/", tag_contacts, name="tag_contacts"),
+    path("not_contacted_campaign/<int:campaign_id>/", not_contacted_campaign, name="not_contacted_campaign"),
 ]
