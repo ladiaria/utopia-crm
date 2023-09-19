a258480f2b169f37aed807e6982a6ba2c00e6de3
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-09-18 23:53:28 -0300

    prepare_merge_to_t766 ready

--- core/migrations/0052_emailreplacement.py
+++ core/migrations/0052_emailreplacement.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0049_product_target_product'),
+        ('core', '0051_address_needs_georef_historicaladdress_needs_georef_and_more'),
     ]
 
     operations = [
