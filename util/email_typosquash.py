f102ba4eaeae95ab1975aa06381f9090f2adbe20
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-09-28 03:56:16 -0300

    support to whitelist domains

--- util/email_typosquash.py
+++ util/email_typosquash.py
@@ -1,6 +1,9 @@
+from pydoc import locate
+
 from validate_email_address import validate_email
 from pymailcheck import split_email, suggest
 
+from django.conf import settings
 from django.core.mail import mail_managers
 from django.utils.translation import gettext as _
 
@@ -18,25 +21,33 @@ def email_replacement_add(domain, replacement):
 
 def clean_email(email):
     """
-    If the email received does not have a valid domain, email returned will be the email given replacing the domain
-    with the replacement existing on our replacement list.
+    If the email received is not whitelisted or not have a valid domain, email returned will be the email given
+    replacing the domain with the replacement existing on our replacement list.
     @returns: a dict with:
       valid: bool,
       email: original email
       replaced: email replaced if match any replacement
       suggestion: suggested email to be used
     """
-    splitted, replacements, valid = split_email(email), EmailReplacement.approved(), bool(validate_email(email, True))
+    splitted = split_email(email)
+    domain, whitelisted_domains = splitted["domain"], getattr(settings, "CORE_WHITELISTED_DOMAINS", [])
+    if type(whitelisted_domains) is str:
+        whitelisted_domains = locate(whitelisted_domains)()
+    whitelisted = domain in whitelisted_domains
 
-    result, domain = {"valid": valid, "email": email}, splitted["domain"]
-    replacement = replacements.get(domain) if replacements else None
-    replaced = "%s@%s" % (splitted["address"], replacement) if replacement else None
+    valid = whitelisted or bool(validate_email(email, True))
+    result = {"valid": valid, "email": email}
 
-    if replaced:
-        result["replacement"] = replaced
-    elif not valid:
-        suggestion = suggest(email)
-        if suggestion and not EmailReplacement.is_rejected(domain, suggestion["domain"]):
-            result["suggestion"] = suggestion["full"]
+    if not whitelisted:
+        replacements = EmailReplacement.approved()
+        replacement = replacements.get(domain) if replacements else None
+        replaced = "%s@%s" % (splitted["address"], replacement) if replacement else None
+
+        if replaced:
+            result["replacement"] = replaced
+        elif not valid:
+            suggestion = suggest(email)
+            if suggestion and not EmailReplacement.is_rejected(domain, suggestion["domain"]):
+                result["suggestion"] = suggestion["full"]
 
     return result
