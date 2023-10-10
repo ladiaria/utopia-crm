341276b65b109783e64b84b6d3acbc7534ea7cde
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-10-10 15:10:46 -0300

    better read-only and delete-only admin model classes

--- core/admin.py
+++ core/admin.py
@@ -402,14 +402,18 @@ class EmailReplacementAdmin(admin.ModelAdmin):
     search_fields = ("domain", "replacement")
 
 
-class ReadOnlyModelAdmin(admin.ModelAdmin):
-    """ A read-only modeladmin, no action can be performed, only see the object list """
-    list_display_links = None
-    actions = None
+class DeleteOnlyModelAdmin(admin.ModelAdmin):
+    """ A delete-only modeladmin, only see and delete is allowed """
 
     def changelist_view(self, request, extra_context=None):
-        extra_context = {'title': self.model._meta.verbose_name_plural.capitalize()}
+        extra_context = extra_context or {}
+        extra_context.update({'title': self.model._meta.verbose_name_plural.capitalize()})
         return super().changelist_view(request, extra_context=extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context.update({'title': self.model._meta.verbose_name.capitalize()})
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
     def has_add_permission(self, request):
         return False
@@ -417,15 +421,23 @@ class ReadOnlyModelAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
-    has_delete_permission = has_change_permission
+
+class ReadOnlyModelAdmin(DeleteOnlyModelAdmin):
+    """ A read-only modeladmin, no action can be performed, only see the object list """
+    list_display_links = None
+    actions = None
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(EmailBounceActionLog)
-class EmailBounceActionLogAdmin(ReadOnlyModelAdmin):
+class EmailBounceActionLogAdmin(DeleteOnlyModelAdmin):
     list_display = ("created", "contact", "email", "action")
     list_filter = ("action", )
     search_fields = ("email", )
     date_hierarchy = "created"
+    fieldsets = ((_("Bounce action log details"), {"fields": (("created", "action"), ("contact", "email"))}), )
 
 
 admin.site.register(DynamicContactFilter)
