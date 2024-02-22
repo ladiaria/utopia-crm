ed9cfb25f041bb78bbad18a7f54f074e3f6d3e39
Trifero <andres.trifero@gmail.com>
2024-02-22 14:21:49 -0300

    API for adding mailtrain emails

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 # coding: utf-8
-from django.http import JsonResponse, HttpResponse
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
 from django.shortcuts import render, get_list_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from core.models import Contact
+from core.utils import subscribe_email_to_mailtrain_list
 
 
 def handler404(request, exception):
@@ -39,5 +42,33 @@ def search_contacts_htmx(request, name="contact"):
         contacts = contacts[:100]
         context = {'contacts': contacts, 'name': name}
         return render(request, "partials/search_contacts_results.html", context)
+    else:
+        return HttpResponse()
+
+
+@csrf_exempt
+def add_email_to_mailtrain_list(request):
+    """
+    View to handle adding emails to a mailtrain list.
+
+    Args:
+    - `request`: Django HttpRequest object.
+
+    Expected POST Parameters:
+    - `email`: Email to be added to the list.
+    - `list_id`: ID of the mailtrain list to add the email to.
+    - `api_key`: Our API key to use for the operation.
+
+    Returns a JSON response with the result of the operation.
+    """
+    if request.POST:
+        if not request.POST.get("api_key", None):
+            return HttpResponseForbidden()
+        if request.POST["api_key"] != getattr(settings, "CRM_API_KEY", None):
+            return HttpResponseForbidden()
+        email = request.POST.get("email")
+        list_id = request.POST.get("list_id")
+        result = subscribe_email_to_mailtrain_list(email, list_id)
+        return HttpResponse(result, content_type="application/json")
     else:
         return HttpResponse()
