5f882695d5e808eee25a21d63b25b1e43664b02c
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-04-01 02:32:44 -0300

    ready_to_test

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TEST_MODULES="tests.test_contact tests.test_dynamiccontactfilter tests.test_invoicing tests.test_subscriptor tests.test_email_replacements"
-# exit now if we are being sourced by another script or shell (or EXIT was on $1)
+# exit now if we are being sourced by another script or shell (or utopia_crm_exit envvar=1 for zsh compatibility)
 [[ "${#BASH_SOURCE[@]}" -gt "1" ]] && { return 0; }
 [[ $utopia_crm_exit = 1 ]] && { return 0; }
 python -W ignore manage.py test --settings=test_settings --keepdb ${TEST_MODULES}
