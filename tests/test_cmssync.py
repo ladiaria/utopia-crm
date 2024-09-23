6eb512d53f0dbc59b359c1f5d6aa3f2f3cab86b4
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-09-22 22:04:34 -0300

    decorator missing added + tottally refactor the fullsynctest (WIP)

--- tests/test_cmssync.py
+++ tests/test_cmssync.py
@@ -2,10 +2,11 @@
 from builtins import range
 import string
 import random
-import requests
 
 from django.conf import settings
 from django.test import TestCase, override_settings
+
+from core.utils import post_to_cms_rest_api
 from tests.factory import create_contact
 
 
@@ -16,75 +17,74 @@ def rand_chars(length=9):
 class CMSyncTestCase(TestCase):
 
     def setUp(self):
-        settings.WEB_UPDATE_USER_VERIFY_SSL = False
-
-    def tearDown(self):
-        # TODO: Review the flow for remove a user from the CMS for tests
-        # It may be sensitive for data inside the CMS
-        pass
-
-    def test_create_contact_sync(self):
-        with override_settings(WEB_UPDATE_USER_ENABLED=True):
+        self.api_key = settings.LDSOCIAL_API_KEY
+        with override_settings(WEB_CREATE_USER_ENABLED=True):
+            # create a user with very low collission probability on email field
             name, email_pre_prefix, phone = "Jane Doe", "cms_test_crmsync_", "12345678"
             email = f"{email_pre_prefix}{rand_chars()}@yoogle.com"
-
-            # create a user with very low collission probability on email field
             self.contact = create_contact(name, phone, email)
-            # get contact from CMS
-            api_url = settings.WEB_EMAIL_CHECK_URI
-            api_key = getattr(settings, "LDSOCIAL_API_KEY", None)
-            if api_url and api_key:
-                res = requests.post(
-                    api_url,
-                    headers={'Authorization': 'Api-Key ' + api_key},
-                    data={"contact_id": self.contact.id, "email": self.contact.email},
-                    verify=settings.WEB_UPDATE_USER_VERIFY_SSL
-                )
-                self.assertEqual(res.json()["msg"], "OK")
-                self.assertEqual(res.json()["retval"], 0)
 
-    def test_delete_contact_sync(self):
-        if self.contact:
-            api_url = settings.WEB_DELETE_USER_URI
-            api_key = getattr(settings, "LDSOCIAL_API_KEY", None)
-            if api_url and api_key:
-                res = requests.delete(
-                    api_url,
-                    headers={'Authorization': 'Api-Key ' + api_key},
-                    data={"contact_id": self.contact.id, "email": self.contact.email},
-                    verify=settings.WEB_UPDATE_USER_VERIFY_SSL
-                )
-                self.assertEqual(res.json()["message"], "OK")
+    def tearDown(self):
+        # TODO: (doing) Review the flow for remove a user from the CMS for tests
+        #       (trying call the deletion api directly
+        """
+        api_uri = settings.WEB_DELETE_USER_URI
+        if api_uri and self.api_key:
+            post_to_cms_rest_api(
+                "delete_contact_sync", api_uri, {"contact_id": self.contact.id, "email": self.contact.email}, "DELETE"
+            )
+        else:
+            print("WARNING: Skipping delete on teardown due to missing API configuration")
+        """
+        pass
 
-                # check of the contact exits in CMS
-                api_url = settings.WEB_EMAIL_CHECK_URI
-                api_key = getattr(settings, "LDSOCIAL_API_KEY", None)
-                if api_url and api_key:
-                    res = requests.post(
-                        api_url,
-                        headers={'Authorization': 'Api-Key ' + api_key},
-                        data={"contact_id": self.contact.id, "email": self.contact.email},
-                        verify=settings.WEB_UPDATE_USER_VERIFY_SSL
-                    )
-                    self.assertEqual(res.json()["msg"], "OK")
-                    self.assertEqual(res.json()["retval"], 0)
+    def test1_create_contact_sync(self):
+        self.assertIsNotNone(self.contact)
+        self.assertIsNotNone(self.contact.id)
+        # check on CMS
+        # TODO: can this endpoint tell us if the user was created?
+        api_uri = settings.WEB_EMAIL_CHECK_URI
+        if api_uri and self.api_key:
+            res = post_to_cms_rest_api(
+                "create_contact_sync", api_uri, {"contact_id": self.contact.id, "email": self.contact.email}
+            ).json()
+            self.assertEqual(res.get("msg"), "OK")
+            self.assertEqual(res.get("retval"), 0)
+        else:
+            print("WARNING: Skipping test_create_contact_sync due to missing API configuration")
 
-    def test_not_create_contact_without_sync(self):
-        with override_settings(WEB_UPDATE_USER_ENABLED=False):
+    def test2_delete_contact_sync(self):
+        self.assertIsNotNone(self.contact)
+        self.assertIsNotNone(self.contact.id)
+        self.contact.delete()
+
+        # check if the user related to the contact exists in CMS
+        # TODO: can this endpoint tell us if the user was deleted?
+        api_uri = settings.WEB_EMAIL_CHECK_URI
+        if api_uri and self.api_key:
+            res = post_to_cms_rest_api(
+                "check_after_delete", api_uri, {"contact_id": self.contact.id, "email": self.contact.email}
+            ).json()
+            self.assertEqual(res.get("msg"), "OK")
+            self.assertEqual(res.get("retval"), 0)
+        else:
+            print("WARNING: Skipping test_delete_contact_sync due to missing API configuration")
+
+    def test3_not_create_contact_without_sync(self):
+        with override_settings(WEB_CREATE_USER_ENABLED=False):
             name, email_pre_prefix, phone = "Jane Doe", "cms_test_crmsync_", "12345678"
             email = f"{email_pre_prefix}{rand_chars()}@yoogle.com"
 
             # create a user with very low collission probability on email field
             no_sync_conctact = create_contact(name, phone, email)
-            # get the contact from CMS
+            # check on CMS
+            # TODO: can this endpoint tell us if the user was not created?
             api_url = settings.WEB_EMAIL_CHECK_URI
-            api_key = getattr(settings, "LDSOCIAL_API_KEY", None)
-            if api_url and api_key:
-                res = requests.post(
-                    api_url,
-                    headers={'Authorization': 'Api-Key ' + api_key},
-                    data={"contact_id": no_sync_conctact.id, "email": no_sync_conctact.email},
-                    verify=settings.WEB_UPDATE_USER_VERIFY_SSL
-                )
-                self.assertEqual(res.json()["msg"], "OK")
-                self.assertEqual(res.json()["retval"], 0)
+            if api_url and self.api_key:
+                res = post_to_cms_rest_api(
+                    "sync_disabled", api_url, {"contact_id": no_sync_conctact.id, "email": no_sync_conctact.email}
+                ).json()
+                self.assertEqual(res.get("msg"), "OK")
+                self.assertEqual(res.get("retval"), 0)
+            else:
+                print("WARNING: Skipping test_not_create_contact_without_sync due to missing API configuration")
