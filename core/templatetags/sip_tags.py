6fbe180eb2d529665746bae4dbc5c7b6a11cc455
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-10-08 13:46:29 -0300

    useful new filter

--- core/templatetags/sip_tags.py
+++ core/templatetags/sip_tags.py
@@ -9,6 +9,15 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
+@register.filter(name='national_number')
+def national_number(value):
+    """
+    @param value: phonenumber_field.phonenumber.PhoneNumber instance
+    @return: national number
+    """
+    return normalize_digits_only(value.as_national) if value else ''
+
+
 @register.filter(name='sip')
 def sip_filter(value):
     """
@@ -16,7 +25,7 @@ def sip_filter(value):
     @return: html with a button to call the phone number using SIP
     """
     # TODO: think what to do if value is not a valid phone number
-    normalized_number = normalize_digits_only(value.as_national)
+    normalized_number = national_number(value)
     return mark_safe(
         '%s <a class="button btn-sm btn-primary" href="sip://%s%s"><i class="fas fa-phone"></i> Llamar</a>' % (
             normalized_number, getattr(settings, 'SIP_DIALOUT', ''), normalized_number
