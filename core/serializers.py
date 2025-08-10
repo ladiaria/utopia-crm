e3d30df7dae66c338a9322b2643ce360179bb8ad
Aníbal Pacheco <apacheco.uy@gmail.com>
2025-08-10 07:36:22 -0300

    doctype serializer wip

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from core.models import Product
+from core.models import Product, IdDocumentType
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class IdDocumentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IdDocumentType
         fields = "__all__"
